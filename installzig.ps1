cfddc29b12d6862c6706f60c554ed95ca0a5a69c
Curt Bushko <cbushko@gmail.com>
2023-09-28 21:16:30 -0400

    ughh 1

--- installzig.ps1
+++ installzig.ps1
@@ -2,7 +2,7 @@ $version = "zig-windows-x86_64-0.12.0-dev.640+937138cb9"
 $uri = "https://ziglang.org/builds/$version.zip"
 Invoke-WebRequest -Uri "$uri" -OutFile ".\zig-windows.zip"
 Expand-Archive -Path ".\zig-windows.zip" -DestinationPath ".\" -Force
-Remove-Item -Path " .\zig-windows1.zip"
+Remove-Item -Path " .\zig-windows.zip"
 Rename-Item -Path ".\$version" -NewName ".\zig"
 
 Write-Host "Zig installed."
