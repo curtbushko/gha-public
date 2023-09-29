b99ebd5fb17fe4898b43111afc0e9edbb2a247fd
Curt Bushko <cbushko@gmail.com>
2023-09-28 21:12:46 -0400

    try newer version

--- installzig.ps1
+++ installzig.ps1
@@ -1,7 +1,8 @@
-Invoke-WebRequest -Uri "https://ziglang.org/download/0.10.1/zig-windows-x86_64-0.10.1.zip" -OutFile ".\zig-windows-x86_64-0.10.1.zip"
-Expand-Archive -Path ".\zig-windows-x86_64-0.10.1.zip" -DestinationPath ".\" -Force
-Remove-Item -Path " .\zig-windows-x86_64-0.10.1.zip"
-Rename-Item -Path ".\zig-windows-x86_64-0.10.1" -NewName ".\zig"
+https://ziglang.org/builds/zig-windows-x86_64-0.12.0-dev.640+937138cb9.zip
+Invoke-WebRequest -Uri "https://ziglang.org/builds/zig-windows-x86_64-0.12.0-dev.640+937138cb9.zip" -OutFile ".\zig-windows.zip"
+Expand-Archive -Path ".\zig-windows.zip" -DestinationPath ".\" -Force
+Remove-Item -Path " .\zig-windows1.zip"
+Rename-Item -Path ".\zig-windows-x86_64-0.12.0-dev.640+937138cb9" -NewName ".\zig"
 
 Write-Host "Zig installed."
 ./zig/zig.exe version
