4f30296f26140e3a35f80788f1b2684a5bd05683
Curt Bushko <cbushko@gmail.com>
2023-09-28 21:15:06 -0400

    try variables

--- installzig.ps1
+++ installzig.ps1
@@ -1,8 +1,9 @@
-https://ziglang.org/builds/zig-windows-x86_64-0.12.0-dev.640+937138cb9.zip
-Invoke-WebRequest -Uri "https://ziglang.org/builds/zig-windows-x86_64-0.12.0-dev.640+937138cb9.zip" -OutFile ".\zig-windows.zip"
+$version = "zig-windows-x86_64-0.12.0-dev.640+937138cb9"
+$uri = "https://ziglang.org/builds/$version.zip"
+Invoke-WebRequest -Uri "$uri" -OutFile ".\zig-windows.zip"
 Expand-Archive -Path ".\zig-windows.zip" -DestinationPath ".\" -Force
 Remove-Item -Path " .\zig-windows1.zip"
-Rename-Item -Path ".\zig-windows-x86_64-0.12.0-dev.640+937138cb9" -NewName ".\zig"
+Rename-Item -Path ".\$version" -NewName ".\zig"
 
 Write-Host "Zig installed."
 ./zig/zig.exe version
